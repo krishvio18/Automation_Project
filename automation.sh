95a4abd318e454847384da98e87c4732e9aa7efc
root <root@ip-172-31-89-77.ec2.internal>
2022-07-08 11:38:27 +0000

    edited s3 bucket

--- automation.sh
+++ automation.sh
@@ -2,7 +2,7 @@
 
 # Variables
 name="krishna"
-s3_bucket="your_bucket"
+s3_bucket="upgrad-krishna"
 
 # update the ubuntu repositories
 apt update -y
@@ -37,26 +37,23 @@ tar -cf /tmp/${krishna}-httpd-logs-${timestamp}.tar *.log
 # copy logs to s3 bucket
 if [[ -f /tmp/${krishna}-httpd-logs-${timestamp}.tar ]]; then
 	#statements
-	aws s3 cp /tmp/${krishna}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
+	aws s3 cp /tmp/${krishna}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${krishna}-httpd-logs-${timestamp}.tar
 
 fi
 docroot="/var/www/html"
-#check if inventory files excits 
-if [[ ! -f ${docroot}/inventory.html ]]; then 
- #statements 
-  echo -e ' Log type \t-\tTime Created\t-\tType\t-\tSize' > ${docroot}/inventory.html
-fi 
+if [[ ! -f ${docroot}/inventory.html ]]; then
+echo -e 'Log Type\t-\tTime Created\t-\tTypeSize' > ${docroot}/inventory.html
+fi
 
-#Inserting Logs into the file 
- if [[ -f ${docroot}/inventory.html ]]; then 
-  #statements 
-size=$(du -h /temp/${name}-httpd-logs-${timestamp}.tar | awk '{print $1}')
-  echo -e "httpd-logs\t-\t${timestamp}\t-\ttar\t-t${size}" >> ${docroot}/inventory.html 
-fi 
+if [[ -f ${docroot}/inventory.html ]]; then
+echo -e 'Log Type\t-\tTime Created\t-\tType\t-\tSize' > ${docroot}/inventory.html
+fi
 
-#create a cron job that runs service every minutes/day 
+if [[ -f ${docroot}/inventory.html ]]; then
+size=$(du -h /tmp/${name}-httpd-logs-${timestamp}.tar | awk '{print $1}')
+echo -e "httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${docroot}/inventory.html
+fi
 
-if [[ -f /etc/cron.d/automation ]] ; then 
- #statement 
-echo " * * * * * root /root/automation.sh" >> /etc/cron.d/automation
+if [[ ! -f /etc/cron.d/automation ]]; then
+echo "* * * * * root /root/automation.sh" >> /etc/cron.d/automation
 fi
